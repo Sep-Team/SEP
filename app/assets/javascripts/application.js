340a099c19a81834c7c37105a6daa6835fd3265d
gregory1308 <gregory1308@misena.edu.co>
2013-12-27 11:31:10 -0500

    form table student

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,12 @@
 //= require bootstrap-datepicker
 //= require_tree .
 
+$(document).ready(function() {
+  //$('a#show_member_link').lazybox({closeImg: true});
+    
+  $('a[rel*=lazybox]').lazybox({close: false, modal: false, opacity: 0.7, klass: 'lazybox', speed: 200});
+  $.lazybox.settings = {cancelClass: "button green", submitClass: 'button red', closeImg: false, niceClose: false}
+  $.rails.allowAction = $.lazybox.confirm;
+ 
+});
+$('#teams').tooltip('show')
